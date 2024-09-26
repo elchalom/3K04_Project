0903229f16da8b3dbf48f1f5757831dc123c5302
Ayaan Khan <khana491@mcmaster.ca>
2024-09-26 15:55:12 -0400

    basic gui implementation

--- DCM/file1.py
+++ DCM/file1.py
@@ -1,1 +1,19 @@
-# from PyQt6.QtWidgets import QApplication, QLabel, QWidget
+import sys
+
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget
+
+# Create an instance of QApplication
+app = QApplication([])
+
+# Create your application's GUI
+window = QWidget()
+window.setWindowTitle("The Fitness®: Pacemaker Monitor")
+window.setGeometry(100, 100, 280, 80)
+helloMsg = QLabel("<h1>Matthew is a cutie patootie!</h1>", parent=window)
+helloMsg.move(60, 15)
+
+# Show your application's GUI
+window.show()
+
+# Run your application's event loop
+sys.exit(app.exec())
