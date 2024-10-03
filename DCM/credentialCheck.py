4af2c83b8ff8e9d7961f7e1c715b99eb4204b6fd
elchalom <elchalom@mcmaster.ca>
2024-10-03 15:53:08 -0400

    Cracked return statemetns, edge cases defeated

--- DCM/credentialCheck.py
+++ DCM/credentialCheck.py
@@ -4,6 +4,12 @@ import os
 file = os.path.join("DCM", "credentials.txt")
 
 def checkCredentials(username : str, password: str) -> bool :
+    if ('' == username | ' ' in username | ',' in username):
+        return False
+    if ('' == password | ' ' in password | ',' in password):
+        return False
+        
+    
     credentials = pd.read_csv(file)
     
     for i in range(credentials.size):
@@ -15,19 +21,24 @@ def checkCredentials(username : str, password: str) -> bool :
     
     return False
 
-def registerUser(username: str, password: str) -> bool:
+def registerUser(username: str, password: str) -> tuple[bool, str]:
+    if  ('' == username | ' ' in username | ',' in username):
+        return (False, "Username contains invalid characters")
+    if ('' == password | ' ' in password | ',' in password):
+        return (False, "Password contains invalid characters")
+    
     credentials = pd.read_csv(file)
     size = credentials['Username'].size
     if size >= 10:
-        return False
+        return (False, "Maximum Number of profiles reached")
     for i in range(size):
         if username == credentials['Username'][i]:
         # Checks if credentials already exis
-            return False
+            return (False, "Username Taken")
     print(credentials)    
     credentials.loc[i+1,'Username'] = username
     credentials.loc[i+1,'Password'] = password
     
     credentials.to_csv(file, index = False)
-    return True
+    return (True, "Registration Successful")
     
