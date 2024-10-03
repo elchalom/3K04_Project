8d702cd6cd5f7ce6e3aefc91640a2ff42bd9a4b9
elchalom <elchalom@mcmaster.ca>
2024-10-03 16:44:51 -0400

    Temporary files to save parameters

---
+++ DCM/parameters.py
@@ -0,0 +1,11 @@
+class heart():
+    def __init__(self) -> None:
+        self.parameter1 = None
+        self.parameter2 = None
+        self.state = 'Idle'
+    
+    def methodVOO(self):
+        self.state = 'VOO'
+        
+        pass
+    
