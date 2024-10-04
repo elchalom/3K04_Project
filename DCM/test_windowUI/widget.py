f1d9dabea4aae1599121287cfaa97b1bfdadd748
khana491 <144563365+khana491@users.noreply.github.com>
2024-10-04 16:25:01 -0400

    Updated login/registration with functionality

--- DCM/test_windowUI/widget.py
+++ DCM/test_windowUI/widget.py
@@ -1,47 +1,148 @@
 # This Python file uses the following encoding: utf-8
 import sys
+import pandas as pd
+import os
 
 from PySide6.QtWidgets import QApplication, QWidget, QMessageBox
+from ui_form import Ui_LoginScreen
+from ui_home import Ui_HomeScreen
+print("Current Working Directory:", os.getcwd())
+file = os.path.join("credentials.txt")
 
-# Important:
-# You need to run the following command to generate the ui_form.py file
-#     pyside6-uic form.ui -o ui_form.py, or
-#     pyside2-uic form.ui -o ui_form.py
-from ui_form import Ui_Widget
-
-class Widget(QWidget):
+class LoginScreen(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.ui = Ui_Widget()
+        self.ui = Ui_LoginScreen()
         self.ui.setupUi(self)
 
-        # Connect login button clicked signal to the login method
+        # Store the new screen instance as an attribute
+        self.new_screen = None
+
+        # Hide the register screen by default
+        self.ui.register_screen.hide()
+
+        # Connect the register buttons to the function that shows the register screen
+        self.ui.register_screen_button.clicked.connect(self.show_register_screen)
+        self.ui.register_screen_button_2.clicked.connect(self.show_register_screen)
+
+        # Connect the login buttons to the function that shows the login screen
+        self.ui.login_screen_button.clicked.connect(self.show_login_screen)
+        self.ui.login_screen_button_2.clicked.connect(self.show_login_screen)
+
+        # Handle the Login button
         self.ui.login_button.clicked.connect(self.handle_login)
-        # self.ui.register_button.clicked.connect(self.handle_register)
+
+        # Handle the Register button
+        self.ui.register_button.clicked.connect(self.handle_register)
+
+    # Function to show the register screen and hide the login screen
+    def show_register_screen(self):
+        self.ui.login_screen.hide()     # Hide login screen
+        self.ui.register_screen.show()  # Show register screen
+
+    # Function to show the login screen and hide the register screen
+    def show_login_screen(self):
+        self.ui.register_screen.hide()  # Hide register screen
+        self.ui.login_screen.show()     # Show login screen
 
     def handle_login(self):
         # Get the text from the username and password input fields
         username = self.ui.username_input.text()
         password = self.ui.password_input.text()
 
-        # Save the username and password to a txt file
-        with open('user_credentials.txt', 'a') as file:
-            file.write(f"Username: {username}, Password: {password}\n")
+        credentials = pd.read_csv(file)
+        size = credentials['Username'].size
 
-        error_message = f"Welcome, {username}!"
-        # Show a popup message box
-        self.show_popup("Login Successful!", error_message)
+        # Simulate successful login by showing a new screen
+        if username and password:  # Simple check for demonstration
+            if (' ' in username or ',' in username):
+                self.show_popup_warning("Login Failed", "Username contains invalid characters (',' or ' '). Try again.")
+            elif (' ' in password or ',' in password):
+                self.show_popup_warning("Login Failed", "Username contains invalid characters (',' or ' '). Try again.")
+            else:
+                print(range(size))
+                for i in range(size):
+                    print(i)
+                    if username == credentials['Username'][i]:
+                        if password == credentials['Password'][i]:
+                            self.show_new_screen(username)
+                            return True
+                        else:
+                            self.show_popup_warning("Login Failed", "Username or password is invalid.")
+                            return False
+                self.show_popup_warning("Login Failed", "Username or password is invalid.")
+                return False
+        else:
+            self.show_popup_warning("Login Failed", "Please enter both username and password.")
 
-    def show_popup(self, title, message):
+    def handle_register(self):
+        # Get the text from the username and password input fields
+        username = self.ui.reg_username_input.text()
+        password1 = self.ui.reg_password_input_1.text()
+        password2 = self.ui.reg_password_input_2.text()
+
+        credentials = pd.read_csv(file, dtype=str)
+        size = credentials['Username'].size
+
+        # If registration is successful
+        # If there is content in all the input boxes
+        if username and password1 and password2:  # Simple check for demonstration
+            if (password1 != password2):
+                self.show_popup_warning("Registration Failed", "Passwords do not match. Try again.")
+                self.ui.reg_password_input_2.clear()
+            else:
+                if (' ' in username or ',' in username):
+                    self.show_popup_warning("Registration Failed", "Username contains invalid characters (',' or ' '). Try again.")
+                elif (' ' in password1 or ',' in password1):
+                    self.show_popup_warning("Registration Failed", "Password contains invalid characters (',' or ' '). Try again.")
+                elif (len(password1) < 6):
+                    self.show_popup_warning("Registration Failed", "Password too short (minimum 6 character). Try again.")
+                else:
+                    if (size >= 10):
+                        self.show_popup_warning("Registration Failed", "Maximum number of profiles reached")
+                    else:
+                        for i in range(size):
+                            if username == credentials['Username'][i]:
+                            # Checks if credentials already exis
+                                self.show_popup_warning("Registration Failed", "Username taken. Try again.")
+                                return(False)
+                        credentials.loc[size+1,'Username'] = username
+                        credentials.loc[size+1,'Password'] = password1
+
+                        credentials.to_csv(file, index = False)
+                        self.show_popup_info("Registration Successful", "Please login using new credentials.")
+                        self.show_login_screen()
+        else:
+            self.show_popup_warning("Registration Failed", "Please fill out all boxes.")
+
+    def show_popup_info(self, title, message):
         msg_box = QMessageBox()
         msg_box.setWindowTitle(title)
         msg_box.setText(message)
-        msg_box.setIcon(QMessageBox.Warning)  # You can change the icon type (e.g., Warning, Critical, etc.)
+        msg_box.setIcon(QMessageBox.Information)
         msg_box.exec()
+
+    def show_popup_warning(self, title, message):
+        msg_box = QMessageBox()
+        msg_box.setWindowTitle(title)
+        msg_box.setText(message)
+        msg_box.setIcon(QMessageBox.Warning)
+        msg_box.exec()
+
+    def show_new_screen(self, username):
+        # Create a new window/screen and keep a reference to it
+        self.new_screen = HomeScreen(username)
+        self.new_screen.show()
+        self.close()  # Close the current login window
+
+class HomeScreen(QWidget):
+    def __init__(self, username, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_HomeScreen()  # Create an instance of the new screen UI class
+        self.ui.setupUi(self)  # Set up the UI
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    widget = Widget()
+    widget = LoginScreen()
     widget.show()
     sys.exit(app.exec())
-
